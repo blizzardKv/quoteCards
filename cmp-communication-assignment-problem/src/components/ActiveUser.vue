ef9545203c9e1211d4eb85db1a19d02ba946fc7e
blizzardKv <makemewanna@yandex.ru>
2020-11-17 22:23:58 +0300

    add more logic, #6

---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <p>{{ age }}</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ActiveUser"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <input v-model="name">
+      <label>enter your name</label>
+    </div>
+    <div>
+      <input v-model="age">
+      <label>enter your age</label>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "UserData"
+    }
+</script>
+
+<style scoped>
+
+</style>
